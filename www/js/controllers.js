f91df0e6586b24f568299b0c7380f780da6defe1
shavonnzhangg <shavon.zhang@hotmail.com>
2016-11-03 16:11:50 -0700

    added submit button

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,11 +10,21 @@ angular.module('starter.controllers', [])
   console.log('WthrCtrl');
 })
 
-.controller('MntCtrl', function($scope) {
+.controller('MntCtrl',function($scope) {
   $scope.title = 'Mountains';
   console.log('MntCtrl');
   $scope.parks = true ;
   $scope.distance = 50 ;
   $scope.difficulty = 0 ;
   $scope.size = 0 ;
+  $scope.test = false ;
+  $scope.querySize = 0 ;
+  $scope.query = "empty";
+  $scope.submitForm = function()
+  {
+    $scope.querySize = $scope.size ;
+    $scope.query = "SELECT * FROM MOUNTAINS WHERE SIZE = " + $scope.querySize + ";" ;
+    $scope.test = !$scope.test ;
+  }
+  $scope.submitForm() ;
 });
